5bab1b4f0bea7f1bbe7fb964ec79bda6849b1070
Oscar Parra <ojparrap@ufpso.edu.co>
2024-05-03 17:02:56 -0500

    astrovue

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+let counter = ref(5)
+
+function add() {
+    counter.value++
+    console.log(counter)
+}
+</script>
+
+<template>
+    <div @click="add" class="text-white hover:cursor-pointer bg-blue-500 rounded py-1 px-2 max-w-44 text-center select-none">
+        Número de clicks: {{ counter }}
+    </div>
+</template>
